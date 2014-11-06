9467265619514cbd0a977448525b2ce368e95a5b
Thanwa Nooploy <duodecimus@hotmail.com>
2014-11-06 14:09:24 +0700

    add body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
 var app = express();
 var db = mongojs('my_server', ['book']);
 
@@ -10,6 +11,11 @@ app.get('/api/book', function(req, res){
 	db.book.find({}, function(err, docs){
 	res.send(docs);	
 	});
+});
+
+app.post('/api/book', function(req, res){
+	//insert new book
+
 });
 
 var server = app.listen(3000, function () {
