03539e915b91feffe0dd7561a32f9c9ce9fd8a8b
Thanwa Nooploy <duodecimus@hotmail.com>
2014-11-20 13:42:29 +0700

    add factory

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,16 @@
-angular.module("myApp", [])
-.controller('mainCtrl', function($scope, $http){
+angular.module("myApp", [btford.socket-io]) //load module
+.factory('socketIO', function(socketFactory){
+	return socketFactory({
+		ioSocket: io.connect("http://localhost:3000");
+	})
+})
+.controller('mainCtrl', function($scope, $http, socketIO){
 	$scope.books = [];
 	$http.get('/api/book').success(function(data){
 		$scope.books = data;
 	})
+
+	$scope.save = function(){
+		$http.post('/api/book', $scope.bookInstance).success(){}
+	}
 })
