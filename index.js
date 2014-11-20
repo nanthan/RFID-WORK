e6ad6a0200076fd796c7f5df85d8180cffe1b824
Thanwa Nooploy <duodecimus@hotmail.com>
2014-11-20 13:24:43 +0700

    add io and http

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require(socket.io).(http);
 var db = mongojs('my_server', ['book']);
 
 
@@ -20,6 +22,6 @@ app.post('/api/book', function(req, res){
 	});
 });
 
-var server = app.listen(3000, function () {
+var server = http.listen(3000, function () {
   console.log("server is running now")
 })
